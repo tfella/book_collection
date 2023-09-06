8de15036f2f561a96420c59cf05ce589fc6373f8
root <root@localhost>
2023-09-06 01:29:17 +0000

    finished pt1 of lab

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_01_175242) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_005624) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_01_175242) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
+    t.decimal "price"
+    t.datetime "published_date"
   end
 
 end
